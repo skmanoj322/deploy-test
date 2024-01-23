7be2aba6e6e6e4a7f0836eaacdb0f371157f409e
Manoj sk <sk.manoj322@gmail.com>
2024-01-23 23:26:20 +0530

    ok

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -1,3 +1,3 @@
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
+#!/bin/bash
 
 cd /home/ubuntu/deploy-test && git pull origin main && yarn build && pm2 stop next && pm2 start npm --name "next" -- run "start:next"

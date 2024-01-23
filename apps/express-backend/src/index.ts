57f140bc723d3b340777e64a5c865fe8244c9a2b
Manoj sk <sk.manoj322@gmail.com>
2024-01-23 09:58:37 +0530

    test

--- apps/express-backend/src/index.ts
+++ apps/express-backend/src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.post("/", (req: any, res: any) => {
   let parsedUser = UserInput.safeParse(req.body);
   if (!parsedUser.success) {
-    res.send("incorrect inpur2!");
+    res.send("incorrect inpur2");
     return;
   }
   res.send("correct input");
